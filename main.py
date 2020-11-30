782f848d2e93bc59e779de358c860c024dde4e79
dcabrerairache <dcabrerairache1999@gmail.com>
2020-11-30 13:29:53 +0100

    ordenar quien empieza

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import random
 # Lista con tuplas de los valores de las cartas del mazo
-mazo = [(1, 1, 1), (1, 2, 1), (1, 3, 1), (1, 4, 1), (2, 1, 2), (2, 2, 2), (2, 3, 2), (2, 4, 2), (3, 1, 3), (3, 2, 3), (3, 3, 3), (3, 4, 3), (4, 1, 4), (4, 2, 4), (4, 3, 4), (4, 4, 4), (5, 1, 5), (5, 2, 5), (5, 3, 5), (5, 4, 5), (6, 1, 6), (6, 2, 6), (6, 3, 6), (6, 4, 6), (7, 1, 7),
-        (7, 2, 7), (7, 3, 7), (7, 4, 7), (8, 1, 8), (8, 2, 8), (8, 3, 8), (8, 4, 8), (9, 1, 9), (9, 2, 9), (9, 3, 9), (9, 4, 9), (10, 1, 0.5), (10, 2, 0.5), (10, 3, 0.5), (10, 4, 0.5), (11, 1, 0.5), (11, 2, 0.5), (11, 3, 0.5), (11, 4, 0.5), (12, 1, 0.5), (12, 2, 0.5), (12, 3, 0.5), (12, 4, 0.5)]
+#cartas (num,palo(4=oro,3=copas,2=espadas,1=bastos,valor)
+ mazo = [(1, 1, 1), (1, 2, 1), (1, 3, 1), (1, 4, 1), (2, 1, 2), (2, 2, 2), (2, 3, 2), (2, 4, 2), (3, 1, 3), (3, 2, 3), (3, 3, 3), (3, 4, 3), (4, 1, 4), (4, 2, 4), (4, 3, 4), (4, 4, 4), (5, 1, 5), (5, 2, 5), (5, 3, 5), (5, 4, 5), (6, 1, 6), (6, 2, 6), (6, 3, 6), (6, 4, 6), (7, 1, 7),
+         (7, 2, 7), (7, 3, 7), (7, 4, 7), (8, 1, 8), (8, 2, 8), (8, 3, 8), (8, 4, 8), (9, 1, 9), (9, 2, 9), (9, 3, 9), (9, 4, 9), (10, 1, 0.5), (10, 2, 0.5), (10, 3, 0.5), (10, 4, 0.5), (11, 1, 0.5), (11, 2, 0.5), (11, 3, 0.5), (11, 4, 0.5), (12, 1, 0.5), (12, 2, 0.5), (12, 3, 0.5), (12, 4, 0.5)]
 
 jugadores = {}
+prioridad = []
 
 # bucle for para añadir a los jugadores
 while len(jugadores) < 8:
@@ -14,9 +16,12 @@ while len(jugadores) < 8:
     if nombre.isalnum() and nombre[0].isalpha():
         # print(nombre)  # DEBUG
         # le generamos una carta al jugador
-        carta = random.randrange(len(mazo))
+        random_c = random.randrange(len(mazo))
         # agregamos el jugador a la lista, con la carta generada
-        jugadores[nombre] = [mazo[carta]]
+        carta = mazo[random_c]
+        mazo.pop(random_c)
+        prioridad.append([carta[2],carta[1], nombre])
+        jugadores[nombre] = [carta]
         continuar = input("Continuas? (Si, No): ")
         if continuar.upper() == "NO":
             break
@@ -25,3 +30,16 @@ while len(jugadores) < 8:
               "espacios!!")
 
 print(jugadores)  # DEBUG
+
+#repartimos cartas para ver el orden.
+
+len_lista = len(prioridad) - 1
+for i in range(len_lista):
+    for j in range(len_lista-i):
+        for x in prioridad:
+            if prioridad[j] < prioridad[j+1]:
+                aux = prioridad[j]
+                prioridad[j] = prioridad[j+1]
+                prioridad[j+1] = aux
+print(mazo)
+print(prioridad)
